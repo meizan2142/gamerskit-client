29c8e61cd9977dfa1a5343e90681037a00cf9d44
Your Name01~git <saifsahai2002@gmail.com>
2025-03-23 23:41:23 +0600

    Work is going well

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Hero from "./Components/Hero/Hero"
 import Navbar from "./Components/Navbar/Navbar"
+import NewArrivals from "./Pages/Home/HomeSections/NewArrivals/NewArrivals"
 
 function App() {
   return (
     <div>
       <div><Navbar/></div>
       <div><Hero/></div>
-      <div className="mt-32">New Arrivals</div>
+      <div className="mt-32">
+        <NewArrivals/>
+      </div>
     </div>
   )
 }
---
+++ src/Pages/Home/HomeSections/NewArrivals/NewArrivals.jsx
@@ -0,0 +1,17 @@
+const NewArrivals = () => {
+    return (
+        <div>
+            {/* Heading */}
+            <div>
+                <h1>New Arrivals</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, ducimus. Repudiandae molestias eaque qui? Voluptatem nisi commodi libero, molestiae dolores deleniti, obcaecati aliquid ab aperiam illum cumque laboriosam, ipsum veniam.</p>
+            </div>
+            {/* Images */}
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default NewArrivals
