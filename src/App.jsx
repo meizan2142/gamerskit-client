db886d87c172ff4734fd0592acc0e4a806c9053f
Your Name01~git <saifsahai2002@gmail.com>
2025-03-25 04:39:20 +0600

    NewsLetter section works begin

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import Hero from "./Components/Hero/Hero"
 import Navbar from "./Components/Navbar/Navbar"
 import NewArrivals from "./Pages/Home/HomeSections/NewArrivals/NewArrivals"
+import NewsLetter from "./Pages/Home/HomeSections/NewsLetter/NewsLetter"
 
 function App() {
   return (
     <div>
-      <div><Navbar/></div>
-      <div><Hero/></div>
+      <div><Navbar /></div>
+      <div><Hero /></div>
       <div className="mt-20">
-        <NewArrivals/>
+        <NewArrivals />
+      </div>
+      <div className="mt-20">
+        <NewsLetter />
       </div>
     </div>
   )
