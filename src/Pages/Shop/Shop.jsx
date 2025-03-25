bf7f37ca8d383359b9d3387190384bfdacbb4c7c
Your Name01~git <saifsahai2002@gmail.com>
2025-03-26 05:11:18 +0600

    shop hero created

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,6 +1,10 @@
+import { ShopHero } from "../../Sections/ShopSections/ShopHero/ShopHero"
+
 const Shop = () => {
     return (
-        <div>Shop</div>
+        <div className="pt-24 lg:pt-28 xl:pt-32 2xl:pt-36 h-[90vh] sm:h-screen">
+            <ShopHero />
+        </div>  
     )
 }
 
---
+++ src/Sections/ShopSections/ShopHero/ShopHero.jsx
@@ -0,0 +1,11 @@
+export const ShopHero = () => {
+    return (
+        <div className="mx-auto px-4 sm:px-6 md:px-6 lg:px-8 xl:px-20 2xl:px-24">
+            <img
+                src="/ShopHero.jpg"
+                className="w-full h-auto rounded sm:rounded-lg md:rounded-xl xl:rounded-2xl"
+                alt="Shop Hero Image"
+            />
+        </div>
+    )
+}
