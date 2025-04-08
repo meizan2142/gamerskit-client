f2c784b241050dec174238d64d288446c06584f0
Your Name01~git <saifsahai2002@gmail.com>
2025-04-08 11:37:36 +0600

    images URL have been changed

--- src/Sections/HomeSections/NewArrivals/allProducts.js
+++ src/Sections/HomeSections/NewArrivals/allProducts.js
@@ -1,16 +1,3 @@
-import Nissan from '/src/assets/Nissan.png';
-import G2 from '/src/assets/Prestige.jpg';
-import Porsche from '/src/assets/Porsche.png';
-import Sentinels from '/src/assets/Bangkok.jpg';
-import RedBull from '/src/assets/Red Bull.jpg';
-import Ferrari from '/src/assets/Ferrari.jpg';
-import Mercedes from '/src/assets/Mercedes.jpg';
-import SentinelsCotton from '/src/assets/sen-cotton.jpg';
-import TeamLiquidSleeves from '/src/assets/Team Liquid-sleeves.jpg';
-import FnaticSleeves from '/src/assets/Fnatic-Sleeves.jpg';
-import SentinelsSleeves from '/src/assets/Sentinels-sleeves.jpg';
-import FnaticCotton from '/src/assets/Fnatic-cotton.jpg';
-
 export const allProducts = () => {
     return [
         {
@@ -18,105 +5,105 @@ export const allProducts = () => {
             "title": "Nissan GTR Skyline 4WD",
             "price": 3500,
             "description": "Premium noise-canceling headphones with 30-hour battery life and deep bass.",
-            "img": "https://res.cloudinary.com/dyqjzfdwi/image/upload/v1744086544/tjeukk1hx1ob0e7dozpi.jpg"
+            "img": "http://res.cloudinary.com/dyqjzfdwi/image/upload/v1744090158/dchrtpia95xdom4koczu.png"
         },
         {
             "name": 'car',
             "title": "Porsche 911 Drift Car 4WD",
             "price": 3500,
             "description": "Insulated 32oz bottle, keeps drinks cold for 24 hours or hot for 12 hours.",
-            "img": Porsche
+            "img": "http://res.cloudinary.com/dyqjzfdwi/image/upload/v1744090188/xyonqh4ycoq1nuqr2lz6.png"
         },
         {
             "name": 'E-sports',
             "title": "Sentinels Jersey",
             "price": 620,
             "description": "Eco-friendly unisex tee, made from 100% organic cotton for all-day comfort.",
-            "img": Sentinels
+            "img": "http://res.cloudinary.com/dyqjzfdwi/image/upload/v1744090226/swywuhatuli1ycrfpmxo.jpg"
         },
         {
             "name": 'F1',
             "title": "F1 RED BULL 2024",
             "price": 620,
             "description": "Adjustable brightness and color temperature with touch controls and USB charging.",
-            "img": RedBull
+            "img": "http://res.cloudinary.com/dyqjzfdwi/image/upload/v1744090244/hykzbjr1yfst9rdpfov8.jpg"
         },
         {
             "name": 'E-sports',
             "title": "G2 Prestige 2025",
             "price": 650,
             "description": "10,000mAh power bank with fast-charging support for smartphones and tablets.",
-            "img": G2   
+            "img": "http://res.cloudinary.com/dyqjzfdwi/image/upload/v1744090261/vwz7hvuuyftui9hklwid.jpg"   
         },
         {
             "name": 'F1',
             "title": "F1 SHELL 2024",
             "price": 620,
             "description": "10,000mAh power bank with fast-charging support for smartphones and tablets.",
-            "img": Ferrari   
+            "img": "http://res.cloudinary.com/dyqjzfdwi/image/upload/v1744090295/dxg7pnt0mkvok3krt7au.jpg"   
         },
         {
             "name": 'F1',
             "title": "F1 Mercedes 2024",
             "price": 620,
             "description": "10,000mAh power bank with fast-charging support for smartphones and tablets.",
-            "img": Mercedes   
+            "img": "http://res.cloudinary.com/dyqjzfdwi/image/upload/v1744090324/yvy2fizopqvmrvqgtfxm.jpg"   
         },
         {
             "name": 'Sleeves',
             "title": "Sentinels Hand Sleeves",
             "price": 250,
             "description": "10,000mAh power bank with fast-charging support for smartphones and tablets.",
-            "img": SentinelsSleeves   
+            "img": "http://res.cloudinary.com/dyqjzfdwi/image/upload/v1744090347/jntxhiczn1dcmnvcpmez.jpg"   
         },
         {
             "name": 'Sleeves',
             "title": "Fnatic Hand Sleeves",
             "price": 250,
             "description": "10,000mAh power bank with fast-charging support for smartphones and tablets.",
-            "img": FnaticSleeves   
+            "img": "http://res.cloudinary.com/dyqjzfdwi/image/upload/v1744090398/paehys3c7ajyoabxbjuj.jpg"   
         },
         {
             "name": 'Sleeves',
             "title": "Team Liquid Hand Sleeves",
             "price": 250,
             "description": "10,000mAh power bank with fast-charging support for smartphones and tablets.",
-            "img": TeamLiquidSleeves   
+            "img": "http://res.cloudinary.com/dyqjzfdwi/image/upload/v1744090413/sdcclepkucf2bksmdnx6.jpg"   
         },
         {
             "name": 'Mask',
             "title": "Sentinels Mask",
             "price": 70,
             "description": "10,000mAh power bank with fast-charging support for smartphones and tablets.",
-            "img": G2   
+            "img": "http://res.cloudinary.com/dyqjzfdwi/image/upload/v1744090261/vwz7hvuuyftui9hklwid.jpg"   
         },
         {
             "name": 'Mask',
             "title": "G2 Mask",
             "price": 70,
             "description": "10,000mAh power bank with fast-charging support for smartphones and tablets.",
-            "img": G2   
+            "img": "http://res.cloudinary.com/dyqjzfdwi/image/upload/v1744090261/vwz7hvuuyftui9hklwid.jpg"   
         },
         {
             "name": 'Mask',
             "title": "Team Liquid Mask",
             "price": 70,
             "description": "10,000mAh power bank with fast-charging support for smartphones and tablets.",
-            "img": G2   
+            "img": "http://res.cloudinary.com/dyqjzfdwi/image/upload/v1744090261/vwz7hvuuyftui9hklwid.jpg"   
         },
         {
             "name": 'Tshirt',
             "title": "Sen Tshirt",
             "price": 390,
             "description": "10,000mAh power bank with fast-charging support for smartphones and tablets.",
-            "img": SentinelsCotton   
+            "img": "http://res.cloudinary.com/dyqjzfdwi/image/upload/v1744090442/knggbatcvwxtrgluvkak.jpg"   
         },
         {
             "name": 'Tshirt',
             "title": "Fnatic Tshirt",
             "price": 390,
             "description": "10,000mAh power bank with fast-charging support for smartphones and tablets.",
-            "img": FnaticCotton   
+            "img": "http://res.cloudinary.com/dyqjzfdwi/image/upload/v1744090452/yed9ixavoturdlmftm8q.jpg"   
         },
     ];
 };
