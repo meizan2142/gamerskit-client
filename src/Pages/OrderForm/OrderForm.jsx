50972dd9f8ce83ade71a7aacda2d3e4c846ba012
Your Name01~git <saifsahai2002@gmail.com>
2025-04-12 23:16:17 +0600

    working on orders form

--- src/Pages/OrderForm/OrderForm.jsx
+++ src/Pages/OrderForm/OrderForm.jsx
@@ -5,39 +5,35 @@ const OrderForm = () => {
                 <h1 className="font-bold text-3xl">Delivery Details</h1>
             </div>
             {/* Main Div */}
-            <div className="grid grid-cols-1 sm:grid gap-10 sm:grid-cols-1 md:grid-cols-1 md:grid lg:flex lg:justify-between">
+            <div className="flex justify-center grid-cols-1 w-full mx-auto sm:grid gap-8 sm:grid-cols-1 md:grid-cols-1 md:grid lg:flex">
                 {/* Input Fields */}
-                <div>
+                <div className="">
                     <form noValidate="" action="" className="container flex flex-col mx-auto space-y-12">
-                        <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm dark:bg-gray-50">
+                        <fieldset className="grid grid-cols-2 gap-6 p-6 rounded-md shadow-sm">
                             <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
                                 <div className="col-span-full sm:col-span-3">
-                                    <label htmlFor="firstname" className="text-sm">First name</label>
-                                    <input id="firstname" type="text" placeholder="First name" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
-                                </div>
-                                <div className="col-span-full sm:col-span-3">
-                                    <label htmlFor="lastname" className="text-sm">Last name</label>
-                                    <input id="lastname" type="text" placeholder="Last name" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
-                                </div>
-                                <div className="col-span-full sm:col-span-3">
                                     <label htmlFor="email" className="text-sm">Email</label>
-                                    <input id="email" type="email" placeholder="Email" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
+                                    <input id="email" type="email" placeholder="Email" className="w-full rounded-md border-black focus:ring focus:ring-opacity-75 text-black focus:ring-violet-600 p-3 outline" />
+                                </div>
+                                <div className="col-span-full sm:col-span-3">
+                                    <label htmlFor="firstname" className="text-sm">Name</label>
+                                    <input id="firstname" type="text" placeholder="First name" className="w-full rounded-md border-black focus:ring focus:ring-opacity-75 text-black focus:ring-violet-600 p-3 outline" />
                                 </div>
                                 <div className="col-span-full">
-                                    <label htmlFor="address" className="text-sm">Address</label>
-                                    <input id="address" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
+                                    <label htmlFor="address" className="text-sm">Mobile Number</label>
+                                    <input id="address" type="number" placeholder="" className="w-full rounded-md border-black focus:ring focus:ring-opacity-75 text-black focus:ring-violet-600 p-3 outline" />
                                 </div>
-                                <div className="col-span-full sm:col-span-2">
+                                <div className="col-span-full sm:col-span-3">
                                     <label htmlFor="city" className="text-sm">City</label>
-                                    <input id="city" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
+                                    <input id="city" type="text" placeholder="" className="w-full rounded-md border-black focus:ring focus:ring-opacity-75 text-black focus:ring-violet-600 p-3 outline" />
                                 </div>
-                                <div className="col-span-full sm:col-span-2">
-                                    <label htmlFor="state" className="text-sm">State / Province</label>
-                                    <input id="state" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
+                                <div className="col-span-full sm:col-span-3">
+                                    <label htmlFor="state" className="text-sm">Thana</label>
+                                    <input id="state" type="text" placeholder="" className="w-full rounded-md border-black focus:ring focus:ring-opacity-75 text-black focus:ring-violet-600 p-3 outline" />
                                 </div>
-                                <div className="col-span-full sm:col-span-2">
-                                    <label htmlFor="zip" className="text-sm">ZIP / Postal</label>
-                                    <input id="zip" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
+                                <div className="col-span-full">
+                                    <label htmlFor="zip" className="text-sm">Address</label>
+                                    <input id="zip" type="text" placeholder="" className="w-full rounded-md border-black focus:ring focus:ring-opacity-75 text-black focus:ring-violet-600 p-3 outline" />
                                 </div>
                             </div>
                         </fieldset>
@@ -48,7 +44,9 @@ const OrderForm = () => {
                 <hr className="border border-black min-h-screen" />
 
                 {/* Price view */}
-                <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda, tempore dignissimos accusantium inventore facilis sint. Molestias aut tempore est omnis, ipsum ab? Voluptate cupiditate consequuntur minus sequi asperiores at labore?</div>
+                <div>
+                    Sabrina Akter Aksha
+                </div>
             </div>
         </div>
     )
