fd3b73ca97087d770c6b59b66ac3e91de882cd2e
Your Name01~git <saifsahai2002@gmail.com>
2025-04-19 20:45:39 +0600

    AuthProvider, FirebaseConfig, PrivateRoute have setup on my project

---
+++ src/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,70 @@
+import { createContext, useEffect, useState } from "react";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+import auth from "../firebaseConfig/firebase.config";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext(null)
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    // Create User
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    // Sign In
+    const signIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+
+    // LogOut
+    const logOut = () => {
+        setLoading(true)
+        setUser(null)
+        signOut(auth)
+    }
+
+    // update user profile 
+    const updateUserProfile = (name, photo) => {
+        setLoading(true)
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photo
+        })
+    }
+
+    // Observer
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                setUser(currentUser)
+                setLoading(false)
+            }
+            else {
+                setUser(null)
+            }
+        })
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+    const authInfo = {
+        user,
+        createUser,
+        setUser,
+        loading,
+        setLoading,
+        logOut,
+        signIn,
+        updateUserProfile
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
---
+++ src/Pages/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react"
+import { AuthContext } from "../../AuthProvider/AuthProvider"
+import { Navigate, useLocation } from "react-router"
+
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext)
+    const location = useLocation()
+    if (loading) {
+        return <div className="min-h-screen pt-24 flex justify-center">
+            <div className="w-10 h-10 animate-[spin_2s_linear_infinite] rounded-full border-4 border-dashed border-[#FFB300]"></div>
+        </div>
+    }
+    else if (user?.email) {
+        return children
+    }
+    return <Navigate state={location.pathname} to="/login" replace></Navigate>
+}
+
+export default PrivateRoute
