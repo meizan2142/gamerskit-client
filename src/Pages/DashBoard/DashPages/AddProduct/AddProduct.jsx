a45a9c153b9e8946a6a20ccefc57f0fcccf7cedc
Your Name01~git <saifsahai2002@gmail.com>
2025-05-05 00:28:55 +0600

    single product is adding on database

--- src/Pages/DashBoard/DashPages/AddProduct/AddProduct.jsx
+++ src/Pages/DashBoard/DashPages/AddProduct/AddProduct.jsx
@@ -1,7 +1,52 @@
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
+
 const AddProduct = () => {
+    const handleAddProduct = async (e) => {
+        e.preventDefault();
+        const form = e.target;
+
+        // Extract form data
+        const productDetails = {
+            name: form.name.value,
+            title: form.title.value,
+            price: form.price.value,
+            totalSizes: form.elements.totalSizes?.value || "0",
+            leftProducts: form.elements.leftProducts?.value || "0",
+            sizes: {
+                s: form.elements.s?.value || "0",
+                m: form.elements.m?.value || "0",
+                l: form.elements.l?.value || "0",
+                xl: form.elements.xl?.value || "0",
+                xxl: form.elements.xxl?.value || "0",
+                xxxl: form.elements.xxxl?.value || "0",
+                xxxxl: form.elements.xxxxl?.value || "0",
+            },
+            mainImage: form.elements.mainImage.value,
+            subImages: [
+                form.elements.img1.value,
+                form.elements.img2.value,
+                form.elements.img3.value,
+                form.elements.img4.value,
+            ],
+        };
+
+        try {
+            const response = await axios.post(
+                `${import.meta.env.VITE_API_URL}/addedProducts`,
+                productDetails
+            );
+            console.log("Product added successfully:", response.data);
+            toast.success("Product saved successfully!");
+        } catch (error) {
+            console.error("Error saving product:", error);
+            toast.error("Failed to save product.");
+        }
+    };
     return (
         <div>
-            <form className="container flex flex-col mx-auto space-y-8 md:space-y-12">
+            <form onSubmit={handleAddProduct} className="container flex flex-col mx-auto space-y-8 md:space-y-12">
+                <Toaster/>
                 <fieldset className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6 p-4 sm:p-6 rounded-md shadow-sm">
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 gap-4 col-span-full">
                         {/* Product Name and Title */}
@@ -10,6 +55,7 @@ const AddProduct = () => {
                                 <label htmlFor="name" className="text-sm sm:text-base font-bold">Name (small letters)</label>
                                 <input
                                     id="name"
+                                    name="name"
                                     type="text"
                                     placeholder="Name"
                                     className="w-full rounded-md border border-black text-black p-2 sm:p-3 outline-none"
@@ -20,6 +66,7 @@ const AddProduct = () => {
                                 <label htmlFor="title" className="text-sm sm:text-base font-bold">Product Title</label>
                                 <input
                                     id="title"
+                                    name="title"
                                     type="text"
                                     placeholder="Product Title"
                                     className="w-full rounded-md border border-black text-black p-2 sm:p-3 outline-none"
@@ -32,6 +79,7 @@ const AddProduct = () => {
                             <label htmlFor="price" className="text-sm sm:text-base font-bold">Price</label>
                             <input
                                 id="price"
+                                name="price"
                                 type="number"
                                 placeholder="Product Price"
                                 className="w-full rounded-md border-black text-black p-2 sm:p-3 outline"
@@ -42,11 +90,12 @@ const AddProduct = () => {
                         <div className="col-span-full grid grid-cols-1 sm:grid-cols-2 gap-4">
                             {/* District */}
                             <div className="w-full">
-                                <label htmlFor="totalProducts" className="text-sm sm:text-base font-bold">Total Products</label>
+                                <label htmlFor="Total SizesProducts" className="text-sm sm:text-base font-bold">Total Products</label>
                                 <input
                                     type="number"
-                                    id="totalProducts"
-                                    placeholder="Total Products"
+                                    id="totalSizes"
+                                    name="totalSizes"
+                                    placeholder="Total Sizes Products"
                                     className="w-full rounded-md border border-black text-black p-2 sm:p-3 outline-none" />
                             </div>
 
@@ -54,19 +103,22 @@ const AddProduct = () => {
                             <div className="w-full">
                                 <label htmlFor="thana" className="text-sm sm:text-base font-bold">Left Products</label>
                                 <input
-                                    type="text"
+                                    type="number"
+                                    id="leftProducts"
+                                    name="leftProducts"
                                     placeholder="Left Products"
                                     className="w-full rounded-md border border-black text-black p-2 sm:p-3 outline-none" />
                             </div>
                         </div>
+                        {/* Sizes */}
                         <div className="col-span-full grid grid-cols-1 sm:grid-cols-2 gap-4">
-                            {/* District */}
                             <div className="min-w-[120px] flex-1">
                                 <label htmlFor="s" className="text-sm sm:text-base font-bold block">Size: S</label>
                                 <input
                                     type="number"
+                                    name="s"
                                     id="s"
-                                    placeholder="Total"
+                                    placeholder="Total S Sizes"
                                     className="w-full rounded-md border border-black text-black p-2 sm:p-3 outline-none"
                                 />
                             </div>
@@ -74,8 +126,9 @@ const AddProduct = () => {
                                 <label htmlFor="s" className="text-sm sm:text-base font-bold block">Size: M</label>
                                 <input
                                     type="number"
-                                    id="s"
-                                    placeholder="Total"
+                                    name="m"
+                                    id="m"
+                                    placeholder="Total M Sizes"
                                     className="w-full rounded-md border border-black text-black p-2 sm:p-3 outline-none"
                                 />
                             </div>
@@ -83,8 +136,9 @@ const AddProduct = () => {
                                 <label htmlFor="s" className="text-sm sm:text-base font-bold block">Size: L</label>
                                 <input
                                     type="number"
-                                    id="s"
-                                    placeholder="Total"
+                                    name="l"
+                                    id="l"
+                                    placeholder="Total L Sizes"
                                     className="w-full rounded-md border border-black text-black p-2 sm:p-3 outline-none"
                                 />
                             </div>
@@ -92,8 +146,9 @@ const AddProduct = () => {
                                 <label htmlFor="s" className="text-sm sm:text-base font-bold block">Size: XL</label>
                                 <input
                                     type="number"
-                                    id="s"
-                                    placeholder="Total"
+                                    name="xl"
+                                    id="xl"
+                                    placeholder="Total XL Sizes"
                                     className="w-full rounded-md border border-black text-black p-2 sm:p-3 outline-none"
                                 />
                             </div>
@@ -101,8 +156,9 @@ const AddProduct = () => {
                                 <label htmlFor="s" className="text-sm sm:text-base font-bold block">Size: XXL</label>
                                 <input
                                     type="number"
-                                    id="s"
-                                    placeholder="Total"
+                                    name="xxl"
+                                    id="xxl"
+                                    placeholder="Total XXL Sizes"
                                     className="w-full rounded-md border border-black text-black p-2 sm:p-3 outline-none"
                                 />
                             </div>
@@ -110,8 +166,9 @@ const AddProduct = () => {
                                 <label htmlFor="s" className="text-sm sm:text-base font-bold block">Size: 3XL</label>
                                 <input
                                     type="number"
-                                    id="s"
-                                    placeholder="Total"
+                                    name="xxxl"
+                                    id="xxxl"
+                                    placeholder="Total 3XL Sizes"
                                     className="w-full rounded-md border border-black text-black p-2 sm:p-3 outline-none"
                                 />
                             </div>
@@ -119,15 +176,17 @@ const AddProduct = () => {
                                 <label htmlFor="s" className="text-sm sm:text-base font-bold block">Size: 4XL</label>
                                 <input
                                     type="number"
-                                    id="s"
-                                    placeholder="Total"
+                                    name="xxxxl"
+                                    id="xxxxl"
+                                    placeholder="Total 4XL Sizes"
                                     className="w-full rounded-md border border-black text-black p-2 sm:p-3 outline-none"
                                 />
                             </div>
                             <div className="min-w-[120px] flex-1">
                                 <label htmlFor="mainImage" className="text-sm sm:text-base font-bold block">Main Image (URL)</label>
                                 <input
-                                    type="text"
+                                    type="url"
+                                    name="mainImage"
                                     id="mainImage"
                                     placeholder="Main Image (URL)"
                                     className="w-full rounded-md border border-black text-black p-2 sm:p-3 outline-none"
@@ -136,35 +195,39 @@ const AddProduct = () => {
                         </div>
                         {/* Sub Images URL */}
                         <div className="col-span-full sm:col-span-3">
-                            <label htmlFor="price" className="text-sm sm:text-base font-bold">Sub Images</label>
+                            <label htmlFor="img1" className="text-sm sm:text-base font-bold">Sub Images</label>
                             <input
-                                id="price"
-                                type="number"
+                                name="img1"
+                                id="img1"
+                                type="url"
                                 placeholder="1"
                                 className="w-full rounded-md border-black text-black p-2 sm:p-3 outline"
                             />
                         </div>
                         <div className="col-span-full sm:col-span-3">
                             <input
-                                id="price"
-                                type="number"
+                                name="img2"
+                                id="img2"
+                                type="url"
                                 placeholder="2"
                                 className="w-full rounded-md border-black text-black p-2 sm:p-3 outline"
                             />
                         </div>
                         <div className="col-span-full sm:col-span-3">
                             <input
-                                id="price"
-                                type="number"
+                                type="url"
+                                id="img3"
+                                name="img3"
                                 placeholder="3"
                                 className="w-full rounded-md border-black text-black p-2 sm:p-3 outline"
                             />
                         </div>
                         <div className="col-span-full sm:col-span-3">
                             <input
-                                id="price"
-                                type="number"
+                                type="url"
                                 placeholder="4"
+                                id="img4"
+                                name="img4"
                                 className="w-full rounded-md border-black text-black p-2 sm:p-3 outline"
                             />
                         </div>
