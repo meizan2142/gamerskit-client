ac07b31ef458e9643b484bf297768a2056805c1d
Your Name01~git <saifsahai2002@gmail.com>
2025-07-22 02:06:31 +0600

    accounts hidden

--- src/Pages/DashBoard/DashBoard.jsx
+++ src/Pages/DashBoard/DashBoard.jsx
@@ -28,14 +28,14 @@ const DashBoard = () => {
                             Home
                         </NavLink>
 
-                        <NavLink
+                        {/* <NavLink
                             to='/dashboard/accounts'
                             className={({ isActive }) =>
                                 `w-full py-2 px-4 mb-3 rounded ${isActive ? 'bg-green-600 text-white' : 'bg-[#333333] text-white'}`
                             }
                         >
                             Accounts
-                        </NavLink>
+                        </NavLink> */}
 
                         <NavLink
                             to='/dashboard/add-product'
@@ -137,14 +137,14 @@ const DashBoard = () => {
                         >
                             <button className="w-full py-2 px-4 rounded text-white">Home</button>
                         </NavLink>
-                        <NavLink
+                        {/* <NavLink
                             to='/dashboard/accounts'
                             className={({ isActive }) =>
                                 `w-full py-2 px-4 mb-3 rounded ${isActive ? 'bg-green-600 text-white' : 'bg-[#333333] text-white'}`
                             }
                         >
                             Accounts
-                        </NavLink>
+                        </NavLink> */}
                         <NavLink to='/dashboard/all-orders' onClick={() => setIsMobileMenuOpen(false)}
                             className={({ isActive }) =>
                                 `w-full py-2 px-4 mb-3 rounded ${isActive ? 'bg-green-600 text-white' : 'bg-[#333333] text-white'}`
